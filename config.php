8e2bc07185416b47f6c7db1b3e7d0cea4a7aab47
Ahmed <w7orld@gmail.com>
2026-06-24 15:22:33 +0400

    fix api key

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ define('TAP_API_BASE',   'https://api.tap.company/v2');
 define('TAP_CURRENCY',   'SAR');
 
 // Change this to your actual domain when deploying
-define('APP_URL', 'https://tap-wallet.test');
-// define('APP_URL', 'https://tap-wallet-dyrvzqxm.on-forge.com');
+// define('APP_URL', 'https://tap-wallet.test');
+define('APP_URL', 'https://tap-wallet-dyrvzqxm.on-forge.com');
 define('CALLBACK_URL', APP_URL . '/callback.php');
 
 define('DB_PATH', __DIR__ . '/wallet.db');
